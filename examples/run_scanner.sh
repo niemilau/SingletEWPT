637912b3c44d92b2b789084f6c2d632a2bff772c
xiagt-summer <libranum1022@gmail.com>
2023-10-26 19:06:28 +0800

    auto submit scanner jobs based on mh2

--- examples/run_scanner.sh
+++ examples/run_scanner.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 # The directory structure should be like run/mh2-$mh2/<splitted a2>, which is based on the PrepareBigScan.sh
-# In the directory run/, call this script and it will go through all leaf directories.
+# In the directory run/, call this script with three arguments mh2_min, mh2_max and mh2_delta, 
+# it will go through all directories in the mh2-$mh2 with step mh2_delat from [mh2_min, mh2_max].
 # If the leaf directory has submit_scanner.job and parameters file for SingletEWPT, submit the job in the INPAC-Cluster
+if [ "$#" -ne 3 ]; then
+    echo "Usage: <script name> mh2_min mh2_max mh2_delta"
+	exit 1
+fi
+
+mh2_min=$1
+mh2_max=$2
+mh2_delta=$3
+
 scanner_job='submit_scanner.job'
 paramfile='parameters'
-for dir in */
+
+
+for mh2 in $(seq $mh2_min $mh2_delta $mh2_max);
 do
-    if [[ $dir == "*/" ]]; then
+    dir="mh2-$mh2"
+    if [ ! -d "$dir" ]; then
+  		echo "Error: No directory $dir"
         continue
     fi
     cd $dir
-
     for subdir in */
     do
         if [[ $subdir == "*/" ]]; then
