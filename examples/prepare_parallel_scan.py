bd8ef5636924408ac381d1903c688186065402d9
Lauri Niemi <lauri.b.niemi@helsinki.fi>
2024-03-22 02:19:51 +0200

    python script for preparing a large parallelized scan

---
+++ examples/prepare_parallel_scan.py
@@ -0,0 +1,95 @@
+import numpy as np
+import itertools
+import os
+import shutil
+
+def makeRangeInclusive(start: float, stop: float, stepSize: float) -> np.ndarray:
+    """Uses smaller step size in the last step if the division is not even"""
+
+    # This is sensitive to floating point errors, the division can be eg. 5.99999 => we get 5, which is wrong. We "fix" this below
+    wholeIntervals = int((stop - start) / stepSize)
+
+    values = [start + i*stepSize for i in range(wholeIntervals)]
+
+    ## Now append enough full step sizes to ensure we reach stop. If we go past it, undo one step and append stop directly 
+    while values[-1] < stop:
+        values.append( values[-1] + stepSize )
+
+    if values[-1] > stop:
+        values[-1] = stop
+
+    return np.array(values)
+
+
+def main():
+
+    values_mh2 = makeRangeInclusive(100, 800, 5)
+    values_a2 = makeRangeInclusive(0, 8, 0.1)
+    values_b4 = makeRangeInclusive(0.25, 2.0, 0.25)
+    values_sintheta = makeRangeInclusive(-0.3, 0.3, 0.02)
+    values_b3 = makeRangeInclusive(-150, 150, 50)
+
+    ## ---- Construct all possible combinations of these and put them in a 2D table
+
+    scanTable = np.array(list( itertools.product(values_mh2, values_a2, values_b4, values_sintheta, values_b3) ))
+
+    ## Parameter names in the same order as in the tuple above
+    paramNames = ["mh2", "a2", "b4", "sinTheta", "b3"]
+
+    ## ---- Prepare N directories, one for each subscan, and copy necessary files there
+    numProcesses = 2
+
+    ## Should we copy and prepare files for slurm batch submission?
+    bSlurmFiles = True
+
+    filesToCopy = ["parameters"]
+
+    if bSlurmFiles:
+        filesToCopy.append("submit.job")
+        # We give each job a unique name, so read the file here for easier replacing later
+        with open("submit.job", "r") as f:
+            jobFileLines = f.readlines()
+        
+        for i, line in enumerate(jobFileLines):
+            if line.startswith("#SBATCH --job-name"):
+                jobNameLineIndex = i
+
+
+    def copyFiles(dirName: str) -> None:
+        # helper function
+        for fname in filesToCopy:
+            pathToCopy = shutil.copy(fname, dirName)
+
+            if fname == "submit.job":
+                # TODO should not actually need to copy this since we have the contents stored
+                jobFileLines[jobNameLineIndex] = (f'#SBATCH --job-name="scan{i+1}"')
+                with open(pathToCopy, "w") as f:
+                    f.writelines(jobFileLines)
+
+
+    ## ---- Slice the parameters table into a smaller 2D table and construct range_<param> files from it, then store in appropriate subdir
+    numRows, numColumns = scanTable.shape
+    rowsPerDir = numRows // numProcesses
+    remainder = numRows % numProcesses
+
+    for i in range(numProcesses):
+        dirName = f"subscan_{i+1}"
+        os.makedirs(dirName, exist_ok=False)
+
+        copyFiles(dirName)
+
+        startRow = i * rowsPerDir
+        endRow = startRow + rowsPerDir + (1 if i < remainder else 0)
+
+        scanTableSlice = scanTable[startRow:endRow, :]
+
+        ## Write each column to a range_<param> file
+        for j in range(numColumns):
+            
+            columnData = scanTableSlice[:, j]
+            filePath = os.path.join(dirName, f"range_{paramNames[j]}")
+            np.savetxt(filePath, columnData)
+
+
+if __name__ == "__main__":
+    main()
